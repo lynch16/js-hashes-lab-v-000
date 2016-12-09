f5e3ac6a24dcbcefff0d6c4ab083562f6e45bc09
lynch16 <will.lynch91@gmail.com>
2016-12-08 20:16:42 -0500

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+var recipe = {};
+
+function addIngredient(recipe, ingr, amounts) {
+  recipe[ingr] = amounts;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingr) {
+  delete recipe[ingr];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingr, amounts) {
+    recipe[ingr] = amounts;
+    return recipe;
+  }
+
+function readRecipe(recipe) {
+  var string = "this recipe calls for ";
+  for (var key in recipe){
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
